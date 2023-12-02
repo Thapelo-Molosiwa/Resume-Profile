157862f773d3690e072778de81116cca690af979
Thapelo-Molosiwa <0728921928p@gmail.com>
2023-12-02 15:55:42 +0200

    Added Skills

--- Resume/components/pages/skills/Skills.jsx
+++ Resume/components/pages/skills/Skills.jsx
@@ -1,15 +1,76 @@
 import React from "react";
+import "./skills.css";
 
-
-
-export default function Skills(){
-
-return(
-    <div className="thapelo">
-        <h1>Thapelos skills</h1>
-        <div className="loading"></div>
+export default function Skills() {
+  return (
+    <div className="skillbox">
+      <div className="skillset">
+        <div>
+          <h1>HARD SKILLS</h1>
+          <ul>
+            <li>JAVASCRIPT</li>
+            <li>REACT.JS</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>NEXT.JS</li>
+          </ul>
+        </div>
+        <div>
+          <h1>SOFT SKILLS</h1>
+          <ul>
+            <li>TEAM - WORK</li>
+            <li>VERBAL COMMUNICATION</li>
+            <li>WRITTEN COMMUNICATION</li>
+            <li>PROBLEM SOLVING</li>
+            <li>ADAPTABILITY AND LEARNING</li>
+          </ul>
+        </div>
+      </div>
+      <br></br>
+      <div className="bothsh">
+        <p>
+          I am a results-driven software developer with a strong foundation in
+          hard technical skills and a proven track record of delivering
+          high-quality projects. Proficient in programming languages such as
+          JavaScript, HTML and CSS I specialize in developing robust and
+          scalable software solutions using frameworks like Next.js and React.
+        </p>
+        <br></br>
+        <p>
+        My expertise extends beyond technical proficiency to encompass vital
+        soft skills that contribute significantly to project success. Known for
+        my exceptional teamwork, I excel in collaborative environments,
+        fostering positive relationships with cross-functional teams and
+        stakeholders. My effective verbal communication ensures clear and
+        efficient exchanges of ideas, facilitating seamless collaboration with
+        team members and enhancing overall project coherence.
+        
+        </p>
+        <br></br>
+        
+        
+        
+        <p>
+        Adept at
+        problem-solving, I approach challenges methodically, analyzing issues
+        and implementing innovative solutions to optimize project outcomes.
+        Furthermore, my strong written communication skills are reflected in
+        detailed documentation and clear project specifications, ensuring that
+        all team members are on the same page and minimizing the risk of
+        misunderstandings.
+        </p>
+        <br></br>
+        
+        <p>
+        In addition to my technical prowess, my commitment to
+        continuous learning allows me to stay ahead of industry trends and
+        technologies, ensuring that I contribute innovative solutions to complex
+        problems. Overall, my combined hard technical skills and strong soft
+        skills make me an invaluable asset to any software development team,
+        ready to excel in collaborative environments and drive projects to
+        successful fruition.
+        </p>
+      </div>
     </div>
-)
-
-
+  );
 }
