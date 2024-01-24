5f1012769475ce7128ce4b0adcd4987e57ad2607
Isaac Thapelo Molosiwa <0728921928p@gmail.com>
2024-01-24 22:42:08 +0000

    Added burger menu

--- Resume/components/Header/Header.jsx
+++ Resume/components/Header/Header.jsx
@@ -1,32 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Header/header.css";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="blow">
+    <header className="blow">
       <div className="myName">
         <Link to="/">Home</Link>
       </div>
 
-      <ul className="myList">
-
-      <li className="navItem">
-          <Link to="/about">About</Link>
-        </li>
-
-        <li className="navItem">
-          <Link to="/skills">Skills</Link>
-        </li>
-
-        
-        <li className="navItem">
-          <Link to="/education">Education</Link>
-        </li>
-        <li className="navItem">
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+      <div
+        className={`burger-menu ${isMenuOpen ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <nav className={`nav ${isMenuOpen ? "active" : ""}`}>
+        <ul className="myList">
+          <li className="navItem">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="navItem">
+            <Link to="/skills">Skills</Link>
+          </li>
+          <li className="navItem">
+            <Link to="/education">Education</Link>
+          </li>
+          <li className="navItem">
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
